ecb0c9a8d474ce50ef3a24b86b3c50a7b1fb2a3d
indiamcq <india.mcq@gmail.com>
2016-02-02 15:03:12 +0800

    with usx.js that puts chapter@number as chapter text()

---
+++ usx.js
@@ -0,0 +1,4 @@
+var chaptcount = document.querySelectorAll("chapter").length;
+for (i = 0; i < chaptcount; i++) {
+  document.getElementsByTagName("chapter")[i].innerHTML = document.getElementsByTagName("chapter")[i].getAttribute("number");
+}
